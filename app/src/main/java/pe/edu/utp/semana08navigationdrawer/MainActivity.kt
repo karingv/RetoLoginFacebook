b27cef79b5d2e57c2c20212805cd6013c569cda5
PedroDavid <u19207574@utp.edu.pe>
2023-12-13 15:48:28 -0500

    [Acivity Registrar y Clima separado, inicia con OP1 ]
    + Clima: datos clima
    + OP1: mantenimiento y datos
    + OP2: mantenimiento

--- app/src/main/java/pe/edu/utp/semana08navigationdrawer/MainActivity.kt
+++ app/src/main/java/pe/edu/utp/semana08navigationdrawer/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.core.view.isGone
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.facebook.login.LoginManager
@@ -35,14 +36,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var drawerPrincipal: DrawerLayout
     private lateinit var toolbar: Toolbar
     private lateinit var nvPrincipal: NavigationView
+
     private lateinit var provider: String
 
+    private lateinit var etCorreo: EditText
+    private lateinit var etName: EditText
+    private lateinit var etProvider: EditText
+    private lateinit var tvTitle: TextView
+
+    private lateinit var btnCerrar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
 //        val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        initData()
 //        splashScreen.setKeepOnScreenCondition{ true }
 
         this.drawerPrincipal = findViewById(R.id.dlPrincipal)
@@ -66,14 +74,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
+    private fun initData() {
+        etCorreo = findViewById(R.id.etCorreo)
+        etName = findViewById(R.id.etName)
+        etProvider = findViewById(R.id.etProvider)
 
-        private fun setup(email: String, provider: String, imagenPerfil: String, name: String){
+        tvTitle = findViewById(R.id.tvTitle)
 
-            title = "main"
-            findViewById<TextView>(R.id.tvTitle).text = "Bienvenido: ${name}"
-            findViewById<EditText>(R.id.etCorreo).setText(email)
-            findViewById<EditText>(R.id.etName).setText(name)
-            findViewById<EditText>(R.id.etProvider).setText(provider)
+        btnCerrar = findViewById(R.id.btnCerrar)
+        Log.d("TAG+++", "Valor de X: ")
+
+    }
+
+
+    private fun setup(email: String, provider: String, imagenPerfil: String, name: String){
+
+            title = "API Clima - Login [Simple-Facebook]"
+            tvTitle.text = "Bienvenido: ${name}"
+            etCorreo.setText(email)
+            etName.setText(name)
+            etProvider.setText(provider)
 
             val mensajeCompleto = "~En mantenimiento: ${name.split(" ").firstOrNull()}~"
             Opc01Fragment.textBienvenido  = if (!mensajeCompleto.isNullOrEmpty()) {
@@ -84,11 +104,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             val navHeader = nvPrincipal.getHeaderView(0)
             val perfil = navHeader.findViewById<ImageView>(R.id.ivPerfil)
+
+            val correo = navHeader.findViewById<TextView>(R.id.tvCorreo)
+            correo.text = email
+
             Log.d("imagen", "URL: $imagenPerfil")
             if (imagenPerfil.isNotEmpty()) {
-                Picasso.get().load(imagenPerfil).into(perfil)}
+                Picasso.get().load(imagenPerfil).into(perfil)
+            }
 
-            findViewById<Button>(R.id.btnCerrar).setOnClickListener {
+            btnCerrar.setOnClickListener {
                 cerrarSession(provider)
             }
 
@@ -125,10 +150,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        etCorreo.isGone = true
+        etName.isGone = true
+        etProvider.isGone = true
         when (item.itemId) {
-            R.id.nav_home -> fragmentTransaction(HomeFragment())
-            R.id.nav_opc01 -> fragmentTransaction(Opc01Fragment())
-            R.id.nav_opc02 -> fragmentTransaction(Opc02Fragment())
+            R.id.nav_home -> {
+                fragmentTransaction(HomeFragment())
+            }
+            R.id.nav_opc01 -> {
+                fragmentTransaction(Opc01Fragment())
+                etCorreo.isGone = false
+                etName.isGone = false
+                etProvider.isGone = false
+            }
+            R.id.nav_opc02 -> {
+                fragmentTransaction(Opc02Fragment())
+        }
         }
         item.isChecked = true
         drawerPrincipal.closeDrawer(GravityCompat.START)
